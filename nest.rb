91a26b67da035ce461d7e1939d6b855a924e633e
Jason L Perry <jasper@ambethia.com>
2009-01-20 19:50:26 -0500

    Mocked up a login screen

---
+++ nest.rb
@@ -0,0 +1,12 @@
+Shoes.app :title => "Nest",
+  :width => 310, :height => 500, :resize => false do
+  
+  stack :width => 120, :margin_left => 90 do
+    image "bird.png", :margin_bottom => 60, :margin_top => 60
+    edit_line :width => 120
+    inscription "Username"
+    edit_line :width => 120
+    inscription "Password"
+    button "Sign In"
+  end
+end
